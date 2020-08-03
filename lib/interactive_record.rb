8ff3adea02f6ec2c0335927cffe39ff2c1546ac4
Mitzi Estrada <m_estrada17@live.com>
2020-08-03 05:48:58 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -57,9 +57,9 @@ class InteractiveRecord
   def self.find_by(attribute_hash)
     value = attribute_hash.values.first
     formatted_value = value.class == Fixnum ? value : "'#{value}'"
-    binding.pry
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute_hash.keys.first} = #{formatted_value}"
     DB[:conn].execute(sql)
+    binding.pry
   end
     
   end
