05db2355a7c34a42936a4b8dc2a9ff3db8b72f59
Mitzi Estrada <m_estrada17@live.com>
2020-08-03 05:30:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,11 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
 
+
 class Student < InteractiveRecord
 
+self.column_names.each do |column_name|
+    attr_accessor column_name.to_sym
+    end
+    
 end
