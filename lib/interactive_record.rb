65cee573934df38ce5e2f83ad8ef59cedbf1f96f
Mitzi Estrada <m_estrada17@live.com>
2020-08-03 05:37:19 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,7 +50,7 @@ class InteractiveRecord
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
   end
   
-  def find_by_name(name)
+  def self.find_by_name(name)
     DB[:conn].execute("SELECT * FROM #{self.table_name} WHERE name = ?", [name])
   end
     
